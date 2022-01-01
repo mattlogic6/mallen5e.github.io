ac9a63935add3fb789d861350d2ac8e5a4bac00f
TheGiddyLimit <thegiddylimit@5etools.com>
2022-01-01 22:33:34 +0000

    v1.147.6

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 importScripts("./js/sw-files.js");
 
-const cacheName = /* 5ETOOLS_VERSION__OPEN */"1.147.5"/* 5ETOOLS_VERSION__CLOSE */;
+const cacheName = /* 5ETOOLS_VERSION__OPEN */"1.147.6"/* 5ETOOLS_VERSION__CLOSE */;
 const cacheableFilenames = new Set(filesToCache);
 
 let isCacheRunning;
